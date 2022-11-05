2dda0cdad25c92ea6c5409bfc54f97a4cab2d841
khusniddinovuz <khusniddinovuz@gmail.com>
2022-11-05 23:51:00 +0500

    Create login screen

--- src/navigation/MainStackNavigator.js
+++ src/navigation/MainStackNavigator.js
@@ -3,14 +3,16 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
 import Clubs from "../screens/Clubs";
 import Club from "../screens/Club";
+import Login from "../screens/Login";
 
 const Stack = createStackNavigator();
 
 const MainStackNavigator = () => {
     return (<NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator initialRouteName='Login'>
             <Stack.Screen name='Clubs' component={Clubs}/>
             <Stack.Screen name='Club' component={Club}/>
+            <Stack.Screen name='Login' component={Login}/>
         </Stack.Navigator>
     </NavigationContainer>)
 };
