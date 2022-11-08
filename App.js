e9e56c910b8b3cca070842b3896fb384a7c55d0c
khusniddinovuz <khusniddinovuz@gmail.com>
2022-11-08 12:08:04 +0500

    create store and connect to App

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Provider as PaperProvider} from "react-native-paper";
 import MainStackNavigator from "./src/navigation/MainStackNavigator";
+import {Provider as ReduxProvider} from "react-redux";
+import store from "./src/redux/store";
 
 const App = () => {
     return (<PaperProvider>
-        <MainStackNavigator/>
+        <ReduxProvider store={store}>
+            <MainStackNavigator/>
+        </ReduxProvider>
     </PaperProvider>);
 };
 
