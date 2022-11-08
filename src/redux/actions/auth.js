fc8052a95f8884a0b11ab44ba62eda8f50124d44
khusniddinovuz <khusniddinovuz@gmail.com>
2022-11-08 12:04:19 +0500

    create login action

---
+++ src/redux/actions/auth.js
@@ -0,0 +1,10 @@
+import {LOGIN_SUCCESS, REGISTER_SUCCESS} from "./types";
+import axios from "axios";
+
+export const login = (user) => (dispatch) => {
+    axios.post('https://newuuclubs.herokuapp.com/api/auth/login/', user).then(data => {
+        dispatch({type: LOGIN_SUCCESS, payload: data.data})
+    }).catch(err => {
+        console.log(err.response.data)
+    })
+};
